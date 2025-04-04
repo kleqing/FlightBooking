b4ba39acf952de97572b6aa5d094b7d3949f7694
kleqing <hotaruu24k@gmail.com>
2025-04-04 16:33:05 +0700

    Add UserConstant

---
+++ Utility/Constants/UserConstant.cs
@@ -0,0 +1,11 @@
+﻿namespace Utility.Constants;
+
+public static class UserConstant
+{
+	public static string UserNotFound = "User not found!";
+	public static string UserAlreadyExists = "User already exists!";
+	public static string UserCreated = "User created successfully!";
+	public static string UserDeleted = "User deleted successfully!";
+	public static string UserUpdated = "User updated successfully!";
+	public static string GetAllUserSuccess = "All users retrieved successfully!";
+}
